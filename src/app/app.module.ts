38e042c33b9d822ff665a8a64f04e6b604a3191b
mario.sanchez <desarrollo2ude@udea.edu.co>
2021-11-24 06:09:19 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './assets/navbar/navbar.component';
+import { FooterComponent } from './assets/footer/footer.component';
+import { IndexComponent } from './assets/index/index.component';
+import { ErrorComponent } from './assets/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    IndexComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
