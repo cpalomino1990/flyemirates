6d78ee7b9e47f7be82a63deef50423e5c5350fef
mario.sanchez <desarrollo2ude@udea.edu.co>
2021-11-24 07:45:09 -0500

    enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './assets/error/error.component';
+import { IndexComponent } from './assets/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent,
+  },{
+    path: 'seguridad',
+    loadChildren: () => import('./modulos/seguridad/seguridad.module').then(m => m.SeguridadModule)
+  },{
+    path: 'aeropuertos',
+    loadChildren: () => import('./modulos/aeropuertos/aeropuertos.module').then(m => m.AeropuertosModule)
+  },{
+    path: 'admin',
+    loadChildren: () => import('./modulos/admin/admin.module').then(m => m.AdminModule)
+  },{
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/index'
+  },{
+    path: 'error',
+    component: ErrorComponent,
+  },{
+    path: '**',
+    redirectTo: '/error'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
