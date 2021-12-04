a3bbebd966559450dbbe5ac6593aac55b7a1fb4a
mario.sanchez <desarrollo2ude@udea.edu.co>
2021-12-04 07:55:13 -0500

    registrar y editar usuarios

--- src/app/modulos/admin/usuarios/create/create.component.ts
+++ src/app/modulos/admin/usuarios/create/create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuarioModelo } from 'src/app/modelos/usuario.model';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-create',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  fgValidacion = this.fb.group({
+    nombre: ['', [Validators.required]],
+    apellidos: ['', [Validators.required]],
+    telefono: ['', [Validators.required, Validators.minLength(6)]],
+    correo: ['', [Validators.required, Validators.email]],
+  });
+
+
+  constructor(private fb: FormBuilder,
+    private usuarioService: UsuarioService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  store(){
+    let usuario = new UsuarioModelo();
+    usuario.nombre = this.fgValidacion.controls["nombre"].value;
+    usuario.apellidos = this.fgValidacion.controls["apellidos"].value;
+    usuario.correo = this.fgValidacion.controls["correo"].value;
+    usuario.telefono = this.fgValidacion.controls["telefono"].value;
+
+    this.usuarioService.store(usuario).subscribe((data: UsuarioModelo)=> {
+      //Muestra el mensaje de registro exitoso
+      Swal.fire('Creado correctamente!', '', 'success')
+      //Redirecciona a la pagina a donde quiero ir
+      this.router.navigate(['/admin/get']);
+    },
+    (error: any) => {
+      console.log(error)
+      alert("Error en el envio");
+    })
   }
 
 }
